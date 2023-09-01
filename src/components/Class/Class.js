c7a94ba069a20eeb3a911b570e35fa666e91fc10
Radhakrishna Vempati <vempatir@uci.edu>
2023-09-01 01:19:52 -0700

    contains rest API usage to get des and name of course

--- src/components/Class/Class.js
+++ src/components/Class/Class.js
@@ -1,9 +1,41 @@
 import "./Class.css";
+import React, {useState, useEffect} from 'react';
+
 
 function Class(props){
+
+    const [classInfo, setClassInfo] = useState({});
+    const url = "https://api.peterportal.org/rest/v0/courses/"
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch(url + props.name.name);
+            const data = await res.json();
+            setClassInfo(data);
+            
+        }
+        fetchData();
+
+    }, [props.name.name]);
+
+    let info;
+    if(classInfo.id) {
+        info = <div className="information">
+            <p id="title">{classInfo.title}</p>
+            <p id="des">{classInfo.description}</p>
+        </div>
+    }
+    else if(classInfo.error){
+        info = <div><p>Oops.. Class Not Found</p></div>
+    }
+    else {
+        info = <div><p>Loading...</p></div>
+    }
+
     return (
         <div className="class">
             {props.name.code} - {props.name.name}
+        <div>{info}</div>
         </div>
     )
 }
